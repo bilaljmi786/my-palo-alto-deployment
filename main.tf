a1022e0e4b5b1360034bf8542d15d66dea860714
bilaljmi786 <bilaljmi786@gmail.com>
2025-01-23 22:32:48 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,84 @@ resource "azurerm_resource_group" "rg" {
 }
 
 // Add your Palo Alto Panorama and Firewall resources here
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "palo-alto-rg"
+}
+
+variable "location" {
+  description = "The Azure region to deploy the resources in"
+  default     = "East US"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "palo-alto-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "palo-alto-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "palo-alto-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "palo-alto-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "palo-alto-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_D3_v2"
+
+  storage_os_disk {
+    name              = "palo-alto-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "paloaltonetworks"
+    offer     = "vmseries1"
+    sku       = "bundle1"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "palo-alto-vm"
+    admin_username = "azureuser"
+    admin_password = "P@ssw0rd123!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
