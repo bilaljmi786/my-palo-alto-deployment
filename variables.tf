4bebe23749d5cfea95a8c06bfb917bbad7868567
bilaljmi786 <bilaljmi786@gmail.com>
2025-01-23 22:40:25 +0530

    Fixed error

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-/*variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-/}
+//variable "resource_group_name" {
+  //description = "The name of the resource group"
+  //type        = string
+//}
 
 //variable "location" {
   //description = "The Azure region where the resources will be deployed"
