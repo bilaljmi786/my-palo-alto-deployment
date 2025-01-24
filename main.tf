38520d0c528e1146ab8af6c4f69c7903975c214a
bilaljmi786 <bilaljmi786@gmail.com>
2025-01-24 11:18:40 +0530

    Updated files

--- main.tf
+++ main.tf
@@ -1,79 +1,63 @@
-// Add your Palo Alto Panorama and Firewall resources here
-
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  default     = "palo-alto-rg"
+provider "azurerm" {
+  features {}
 }
 
-variable "location" {
-  description = "The Azure region to deploy the resources in"
-  default     = "East US"
+resource "azurerm_resource_group" "palo_alto_rg" {
+  name     = "palo-alto-firewall-rg"
+  location = "Central US"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "palo_alto_vnet" {
   name                = "palo-alto-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.palo_alto_rg.location
+  resource_group_name = azurerm_resource_group.palo_alto_rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "palo_alto_subnet" {
   name                 = "palo-alto-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.palo_alto_rg.name
+  virtual_network_name = azurerm_virtual_network.palo_alto_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "palo-alto-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_network_interface" "palo_alto_nic" {
   name                = "palo-alto-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.palo_alto_rg.location
+  resource_group_name = azurerm_resource_group.palo_alto_rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.palo_alto_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_virtual_machine" "palo_alto_vm" {
   name                  = "palo-alto-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_D3_v2"
+  location              = azurerm_resource_group.palo_alto_rg.location
+  resource_group_name   = azurerm_resource_group.palo_alto_rg.name
+  network_interface_ids = [azurerm_network_interface.palo_alto_nic.id]
+  vm_size               = "Standard_DS1_v2"
 
   storage_os_disk {
-    name              = "palo-alto-os-disk"
+    name              = "palo_alto_os_disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   storage_image_reference {
-    publisher = "paloaltonetworks"
+    publisher = "PaloAltoNetworks"
     offer     = "vmseries1"
-    sku       = "bundle1"
+    sku       = "byol"
     version   = "latest"
   }
 
   os_profile {
-    computer_name  = "palo-alto-vm"
-    admin_username = "azureuser"
-    admin_password = "P@ssw0rd123!"
+    computer_name  = "palo-alto"
+    admin_username = "adminuser"
+    admin_password = "P@ssword123!"
   }
 
   os_profile_linux_config {
